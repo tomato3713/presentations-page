1ff599be1556327d9dc0a5e087baa5ccff6fd61b
tomato3713 <tomato3713@outlook.com>
2024-10-08 00:49:35 +0900

    presentations list design

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-// import "./App.css";
 import Data from "./assets/list.json";
+
+const Title: React.FC<{ title: string }> = ({ title }) => {
+  return <h4 className="text-lg">{title}</h4>;
+};
+
+type Proposal = {
+  id: number;
+  title: string;
+  url: string;
+  ogimage: string;
+  event: {
+    date: string;
+  };
+};
+
+type ProposalListProps = Readonly<{
+  proposals: Proposal[];
+}>;
+const ProposalList: React.FC<ProposalListProps> = ({
+  proposals,
+}: ProposalListProps) => {
+  return (
+    <div className="flex gap-4">
+      {proposals.map((slide: Proposal) => {
+        return (
+          <article key={slide.id} className="flex-initial">
+            <a href={slide.url} target="_blank">
+              <img src={slide.ogimage} alt={slide.title} loading="lazy" />
+            </a>
+            <Title title={slide.title} />
+            <p>{slide.event.date}</p>
+          </article>
+        );
+      })}
+    </div>
+  );
+};
 
 function App() {
   const slides = Data.proposal;
 
   return (
     <>
-      <h1>Presentations</h1>
-      <div className="card">
-        {slides.map((slide) => {
-          return (
-            <article>
-              <a href={slide.url} target="_blank">
-                <img src={slide.ogimage} alt={slide.title} loading="lazy" />
-              </a>
-              <h3>{slide.title}</h3>
-              <p>{slide.event.date}</p>
-            </article>
-          );
-        })}
+      <div className="m-1">
+        <h1 className="font-lg font-mono">Presentations</h1>
+        <ProposalList proposals={slides} />
       </div>
     </>
   );
